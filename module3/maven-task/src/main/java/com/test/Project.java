6b6bf418e1ba9f5f2cb283c157e6d2609ccf5476
Demengjkee <ncherepkin@live.com>
2016-08-08 13:01:17 +0300

    update

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -12,10 +12,10 @@ class Project {
   public static void main(String[] args) {
   	System.out.println("Hellow MTN");
 
-  	Runnable test = () -> { System.out.println("up and running"); };
-  	for(int i = 0; i < 10; i++) {
-  	  	new Thread(test).start();
-  	}
+  //	Runnable test = () -> { System.out.println("up and running"); };
+  //	for(int i = 0; i < 10; i++) {
+  //	  	new Thread(test).start();
+  //	}
   }
 
 }
